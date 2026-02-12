2d4e4e468ab009fdcb0bd0a558019ae61a1dcbd3
*Dayana <*dfnarin@siti.com*>
2026-02-12 16:39:14 -0500

    problema8

--- problema8/main.go
+++ problema8/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -14,20 +15,55 @@ func asyncCuadrado(x int) <-chan int {
 	go func() {
 		defer close(ch)
 		// TODO: simular trabajo
-
+		time.Sleep(500 * time.Millisecond)
 		ch <- x * x
 	}()
 	return ch
 }
+func fanIn(canales ...<-chan int) <-chan int {
+	out := make(chan int)
 
+	var wg sync.WaitGroup
+	wg.Add(len(canales))
+
+	for _, c := range canales {
+		wg.Add(1)
+		go func(c <-chan int) {
+			defer wg.Done()
+			for v := range c {
+				out <- v
+			}
+		}(c)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
 func main() {
-	// TODO: crea varios futuros y recolecta sus resultados: f1, f2, f3
+	// TODO: crea varios futuros y recolecta sus resultados:
+	f1 := asyncCuadrado(2)
+	f2 := asyncCuadrado(3)
+	f3 := asyncCuadrado(4)
 
-	// TODO: Opción 1: esperar cada futuro secuencialmente
+	// TODO: Opción 1:
+	fmt.Println("Esperando futuros secuencialmente:")
+	fmt.Println("f1 =", <-f1)
+	fmt.Println("f2 =", <-f2)
+	fmt.Println("f3 =", <-f3)
+	fmt.Println()
+	f1 = asyncCuadrado(5)
+	f2 = asyncCuadrado(6)
+	f3 = asyncCuadrado(7)
 
-	
-	// TODO: Opción 2: fan-in (combinar múltiples canales)
+	// (combinar múltiples canales)
+	fmt.Println("Fan-in(todos juntos):")
+	for v := range fanIn(f1, f2, f3) {
+		fmt.Println("resultado:", v)
+	}
+	fmt.Println("Fin.")
 	// Pista: crea una función fanIn que recibe múltiples <-chan int y retorna un único <-chan int
 	// que emita todos los valores. Requiere goroutines y cerrar el canal de salida cuando todas terminen.
-	
+
 }
