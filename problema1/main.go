0824fda457ccce31add5b7f506edba9c8d5e861a
Dayana gomez <*dfnarin@siti.com*>
2026-02-12 18:33:15 -0500

    problema2

--- problema1/main.go
+++ problema1/main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 )
 
 // Objetivo: Lanzar varias goroutines que imprimen mensajes y esperar a que todas terminen.
-// TODO: Completa los pasos marcados con TODO para entender goroutines y WaitGroup.
+//  Completa los pasos marcados con TODO para entender goroutines y WaitGroup.
 
 func worker(id int, veces int, wg *sync.WaitGroup) {
-	// TODO: asegurar que al finalizar la función se haga wg.Done()
+	defer wg.Done()
+	//  asegurar que al finalizar la función se haga wg.Done()
 
 	for i := 1; i <= veces; i++ {
 		fmt.Printf("[worker %d] hola %d\n", id, i)
 		//TODO: dormir un poco para simular trabajo (p. ej. 100–300 ms)
-		time.Sleep(time.Duration(100000) * time.Millisecond)
+		time.Sleep(time.Duration(100+rand.Intn(200)) * time.Millisecond)
 	}
 }
 
