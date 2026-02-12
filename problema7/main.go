8c26bf2cac1a856065df6c486284d5c2c825dddf
*Dayana <*dfnarin@siti.com*>
2026-02-12 16:37:21 -0500

    problema1

--- problema7/main.go
+++ problema7/main.go
@@ -16,16 +16,21 @@ type trabajo struct {
 }
 
 type resultado struct {
-	ID       int
-	X        int
+	ID        int
+	X         int
 	Procesado int
 }
 
 func worker(id int, jobs <-chan trabajo, results chan<- resultado, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for j := range jobs {
-		// TODO: procesar j (simular trabajo con Sleep)
-
+		// TODO: procesar j (simular trabajo)
+		time.Sleep(100 * time.Millisecond)
+		r := resultado{
+			ID:        j.ID,
+			X:         j.X,
+			Procesado: j.X * j.X,
+		}
 		fmt.Printf("[worker %d] procesa trabajo %d -> %d\n", id, j.ID, r.Procesado)
 		results <- r
 	}
@@ -42,33 +47,33 @@ func main() {
 	var wg sync.WaitGroup
 
 	// TODO: lanzar nWorkers workers
-	wg.Add(nWorkers)
-	for i := 1; i <= nWorkers; i++ {
 
+	for i := 1; i <= nWorkers; i++ {
+		wg.Add(1)
+		go worker(i, jobs, results, &wg)
 	}
 
 	// TODO: productor de trabajos
 	go func() {
 		for i := 1; i <= nTrabajos; i++ {
-
+			j := trabajo{
+				ID: i,
+				X:  i + 10,
+			}
+			jobs <- j
 		}
 		close(jobs) // importante: cerrar para que los workers terminen
 	}()
 
-	// TODO: recolectar resultados en otra goroutine y cerrar results al final
-	var wgCollect sync.WaitGroup
-	wgCollect.Add(1)
+	// recolectar resultados en otra goroutine y cerrar results al final
 	go func() {
-		defer wgCollect.Done()
 		wg.Wait()      // esperar que todos los workers terminen
 		close(results) // entonces cerrar resultados
 	}()
 
 	// Consumidor principal de resultados
 	for r := range results {
-		fmt.Printf("[main] resultado: trabajo %d -> %d\n", r.ID, r.Procesado)
+		fmt.Printf(" resultado recibido: trabajo %d -> %d\n", r.ID, r.Procesado)
 	}
-
-	wgCollect.Wait()
 	fmt.Println("Pool finalizado.")
 }
