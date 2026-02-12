0b55942bc0b986699c23c8d5f8ffc091b814cf7d
Dayana gomez <*dfnarin@siti.com*>
2026-02-12 18:34:39 -0500

    problema1 modificado

--- problema1/main.go
+++ problema1/main.go
@@ -16,7 +16,7 @@ func worker(id int, veces int, wg *sync.WaitGroup) {
 
 	for i := 1; i <= veces; i++ {
 		fmt.Printf("[worker %d] hola %d\n", id, i)
-		//TODO: dormir un poco para simular trabajo (p. ej. 100–300 ms)
+		//: dormir un poco para simular trabajo (p. ej. 100–300 ms)
 		time.Sleep(time.Duration(100+rand.Intn(200)) * time.Millisecond)
 	}
 }
@@ -24,11 +24,11 @@ func worker(id int, veces int, wg *sync.WaitGroup) {
 func main() {
 	var wg sync.WaitGroup
 
-	// TODO: cambiar estos parámetros y observar el intercalado de salidas
+	// : cambiar estos parámetros y observar el intercalado de salidas
 	numGoroutines := 3
 	veces := 5
 
-	// TODO: lanzar varias goroutines, sumar al WG y esperar con wg.Wait()
+	//: lanzar varias goroutines, sumar al WG y esperar con wg.Wait()
 	for id := 1; id <= numGoroutines; id++ {
 		wg.Add(1)
 		go worker(id, veces, &wg)
